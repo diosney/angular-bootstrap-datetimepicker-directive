4f757d9dbe547d985a7136a489b59afec988e0a1
Naiyin <tianxu0836@gmail.com>
2015-04-02 16:58:49 -0700

    Update angular-bootstrap-datetimepicker-directive.js
    Removed the comma and bracket

--- angular-bootstrap-datetimepicker-directive.js
+++ angular-bootstrap-datetimepicker-directive.js
@@ -1,51 +1,43 @@
 'use strict';
-
 angular
-  .module('datetimepicker', [])
-
-  .provider('datetimepicker', function () {
-    var default_options = {};
-
-    this.setOptions = function (options) {
-      default_options = options;
-    };
-
-    this.$get = function () {
-      return {
-        getOptions: function () {
-          return default_options;
-        }
-      };
-    };
-  })
-
-  .directive('datetimepicker', [
-    '$timeout',
-    'datetimepicker',
-    function ($timeout,
-              datetimepicker) {
-
-      var default_options = datetimepicker.getOptions();
-
-      return {
-        require : '?ngModel',
-        restrict: 'AE',
-        scope   : {
-          datetimepickerOptions: '@'
-        },
-        link    : function ($scope, $element, $attrs, ngModelCtrl) {
-          var passed_in_options = $scope.$eval($attrs.datetimepickerOptions);
-          var options = jQuery.extend({}, default_options, passed_in_options);
-
-          $element
-            .on('dp.change', function (on_change_event) {
-              if (ngModelCtrl) {
-                $timeout(function () {
-                  ngModelCtrl.$setViewValue(on_change_event.target.value);
-                });
-              }
-            })
-            .on('dp.show', function (e) {
+    .module('datetimepicker', [])
+    .provider('datetimepicker', function() {
+        var default_options = {};
+        this.setOptions = function(options) {
+            default_options = options;
+        };
+        this.$get = function() {
+            return {
+                getOptions: function() {
+                    return default_options;
+                }
+            };
+        };
+    })
+    .directive('datetimepicker', [
+        '$timeout',
+        'datetimepicker',
+        function($timeout,
+            datetimepicker) {
+            var default_options = datetimepicker.getOptions();
+            return {
+                require: '?ngModel',
+                restrict: 'AE',
+                scope: {
+                    datetimepickerOptions: '@'
+                },
+                link: function($scope, $element, $attrs, ngModelCtrl) {
+                    var passed_in_options = $scope.$eval($attrs.datetimepickerOptions);
+                    var options = jQuery.extend({}, default_options, passed_in_options);
+                    $element
+                        .on('dp.change', function(e) {
+                            if (ngModelCtrl) {
+                                $timeout(function() {
+                                    ngModelCtrl.$setViewValue(e.target.value);
+                                });
+                            }
+                        })
+                        .on('dp.show', function (e) {
                             var that = this;
                             if (ngModelCtrl) {
                                 $timeout(function() {
@@ -53,8 +45,8 @@ angular
                                 });
                             }
                         })
-            .datetimepicker(options);
+                        .datetimepicker(options);
+                }
+            };
         }
-      };
-    }
-  ]);
+    ]);
