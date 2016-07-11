28274c9829e90d9bf21df034d91ab96dae8aaf3f
IvAlex1986 <iv.alex1986@gmail.com>
2016-07-11 20:02:14 +0300

    add active button feature

--- angular-bootstrap-datetimepicker-directive.js
+++ angular-bootstrap-datetimepicker-directive.js
@@ -4,19 +4,19 @@ angular
   .module('datetimepicker', [])
 
   .provider('datetimepicker', function () {
-    var default_options = {};
+      var default_options = {};
 
-    this.setOptions = function (options) {
-      default_options = options;
-    };
-
-    this.$get = function () {
-      return {
-        getOptions: function () {
-          return default_options;
-        }
+      this.setOptions = function (options) {
+          default_options = options;
       };
-    };
+
+      this.$get = function () {
+          return {
+              getOptions: function () {
+                  return default_options;
+              }
+          };
+      };
   })
 
   .directive('datetimepicker', [
@@ -25,48 +25,50 @@ angular
     function ($timeout,
               datetimepicker) {
 
-      var default_options = datetimepicker.getOptions();
+        var default_options = datetimepicker.getOptions();
 
-      return {
-        require : '?ngModel',
-        restrict: 'AE',
-        scope   : {
-          datetimepickerOptions: '@'
-        },
-        link    : function ($scope, $element, $attrs, ngModelCtrl) {
-          var passed_in_options = $scope.$eval($attrs.datetimepickerOptions);
-          var options = jQuery.extend({}, default_options, passed_in_options);
+        return {
+            require: '?ngModel',
+            restrict: 'AE',
+            scope: {
+                datetimepickerOptions: '@'
+            },
+            link: function ($scope, $element, $attrs, ngModelCtrl) {
+                var passed_in_options = $scope.$eval($attrs.datetimepickerOptions);
+                var options = jQuery.extend({}, default_options, passed_in_options);
 
-          $element
-            .on('dp.change', function (e) {
-              if (ngModelCtrl) {
-                $timeout(function () {
-                  ngModelCtrl.$setViewValue(e.target.value);
-                });
-              }
-            })
-            .datetimepicker(options);
+                $element
+                  .on('dp.change', function (e) {
+                      if (ngModelCtrl) {
+                          $timeout(function () {
+                              var target = $(e.target);
+                              target = (target.is('input') || options.inline) ? target : target.find("input");
+                              ngModelCtrl.$setViewValue(target.val());
+                          });
+                      }
+                  })
+                  .datetimepicker(options);
 
-          function setPickerValue() {
-            var date = options.defaultDate || null;
+                function setPickerValue() {
+                    var date = options.defaultDate || null;
 
-            if (ngModelCtrl && ngModelCtrl.$viewValue) {
-              date = ngModelCtrl.$viewValue;
+                    if (ngModelCtrl && ngModelCtrl.$viewValue) {
+                        date = ngModelCtrl.$viewValue;
+                    }
+
+                    $element
+                      .data('DateTimePicker')
+                      .date(date);
+                }
+
+                if (ngModelCtrl) {
+                    ngModelCtrl.$render = function () {
+                        setPickerValue();
+                    };
+                }
+
+                setPickerValue();
             }
-
-            $element
-              .data('DateTimePicker')
-              .date(date);
-          }
-
-          if (ngModelCtrl) {
-            ngModelCtrl.$render = function () {
-              setPickerValue();
-            };
-          }
-
-          setPickerValue();
-        }
-      };
+        };
     }
   ]);
